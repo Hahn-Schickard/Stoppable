f115d0e476b9dd3abc1e107025a8d9de14908b8d
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-08-14 11:06:42 +0200

    use extra stop future-promise pair to wait until run is finished

--- includes/Routine.hpp
+++ includes/Routine.hpp
@@ -17,14 +17,15 @@ struct Routine {
   using ExceptionHandler = std::function<void(const std::exception_ptr&)>;
 
   explicit Routine(const Cycle& cycle, const ExceptionHandler& handler)
-      : exited_(exit_.get_future()), cycle_(cycle), handler_(handler) {}
+      : exited_(exit_.get_future()), stopped_(stop_.get_future()),
+        cycle_(cycle), handler_(handler) {}
 
   Routine(const Routine&) = delete;
   Routine(Routine&& other) = delete;
   Routine& operator=(const Routine&) = delete;
   Routine& operator=(Routine&& other) = delete;
 
-  virtual ~Routine() { stop(); }
+  ~Routine() { stop(); }
 
   void run() noexcept {
     running_.set_value();
@@ -35,20 +36,22 @@ struct Routine {
         handler_(std::current_exception());
       }
     } while (!stopRequested());
+    stop_.set_value();
   }
 
   std::future<void> running() noexcept { return running_.get_future(); }
 
   bool stopRequested() const {
     using namespace std::chrono;
-    return !(exited_.wait_for(0ms) == std::future_status::timeout);
+    return exited_.wait_for(0ms) != std::future_status::timeout;
   }
 
   void stop() noexcept {
     try {
       exit_.set_value();
+      stopped_.wait();
     } catch (const std::future_error&) {
-      // do nothing
+      // do nothing, routine already stopped
     }
   }
 
@@ -56,6 +59,8 @@ private:
   std::promise<void> running_;
   std::promise<void> exit_;
   std::future<void> exited_;
+  std::promise<void> stop_;
+  std::future<void> stopped_;
   Cycle cycle_;
   ExceptionHandler handler_;
 };
