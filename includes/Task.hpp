8daa6c862b55319aeb2ce8e881c3f2603e90ed50
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-08-14 18:18:38 +0200

    add doxygen documentation

--- includes/Task.hpp
+++ includes/Task.hpp
@@ -4,20 +4,47 @@
 #include "Routine.hpp"
 
 namespace Stoppable {
+/**
+ * @brief Convenience wrapper for Stoppable::Routine
+ *
+ */
 struct Task {
   Task(const Routine::Cycle& cycle, const Routine::ExceptionHandler& handler);
 
+  ///@cond
   Task(const Task&) = delete;
   Task(Task&& other) = delete;
   Task& operator=(const Task&) = delete;
   Task& operator=(Task&& other) = delete;
+  ///@endcond
 
+  /**
+   * @brief Destroy the Task object, calls Task::stop() if the task is still
+   * running
+   *
+   */
   ~Task();
 
+  /**
+   * @brief Start running the Stoppable::Routine::run() and wait for the first
+   * Routine::Cycle call
+   *
+   */
   void start() noexcept;
 
+  /**
+   * @brief Check if the task is running
+   *
+   * @return true if Task::start() was called
+   * @return false if Task::start() was never called or if Task::stop() was
+   */
   bool running() const noexcept;
 
+  /**
+   * @brief Stop the Stoppable::Routine::run() and wait for the last
+   * Routine::Cycle call
+   *
+   */
   void stop() noexcept;
 
 private:
